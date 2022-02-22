17aec2a535c1bd7ad3a2806275d3958d29fa8437
Aybars Acar <aybars.dev@gmail.com>
2022-02-22 20:27:07 +1100

    Welcome Page updated

--- Spotify/Controllers/Other/WelcomeViewController.swift
+++ Spotify/Controllers/Other/WelcomeViewController.swift
@@ -14,30 +14,75 @@ class WelcomeViewController: UIViewController {
     
     button.backgroundColor = .white
     button.setTitle("Sign In with Spotify", for: .normal)
-    button.setTitleColor(.blue, for: .normal)
+    button.setTitleColor(.black, for: .normal)
     
     return button
+  }()
+  
+  private let backgroundImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.contentMode = .scaleAspectFill
+    imageView.image = UIImage(named: "albums_background")
+    return imageView
+  }()
+  
+  private let overlayView: UIView = {
+    let view = UIView()
+    view.backgroundColor = .black
+    view.alpha = 0.9
+    return view
+  }()
+  
+  private let logoImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.contentMode = .scaleAspectFit
+    imageView.image = UIImage(named: "logo")
+    return imageView
+  }()
+  
+  private let label: UILabel = {
+    let label = UILabel()
+    label.numberOfLines = 0
+    label.textAlignment = .center
+    label.textColor = .white
+    label.font = .systemFont(ofSize: 32, weight: .semibold)
+    label.text = "Listen to Millions\nof Songs on\nthe go."
+    return label
   }()
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
     title = "Spotify"
-    view.backgroundColor = .systemGreen
+    view.backgroundColor = .black
+    
+    view.addSubview(backgroundImageView)
+    view.addSubview(overlayView)
     
     view.addSubview(signInButton)
     signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+    
+    view.addSubview(label)
+    view.addSubview(logoImageView)
   }
   
   
   override func viewDidLayoutSubviews() {
     super.viewDidLayoutSubviews()
+    
+    backgroundImageView.frame = view.bounds
+    overlayView.frame = view.bounds
+    
     signInButton.frame = CGRect(
       x: 20,
       y: view.height - 50 - view.safeAreaInsets.bottom,
       width: view.width - 40,
       height: 50
     )
+    
+    logoImageView.frame = CGRect(x: (view.width - 120) / 2, y: (view.height - 350) / 2, width: 120, height: 120)
+    
+    label.frame = CGRect(x: 30, y: logoImageView.bottom + 30, width: view.width - 60, height: 150)
   }
   
   
